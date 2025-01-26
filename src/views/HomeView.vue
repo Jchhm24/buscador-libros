bd907388315e14331ceb2a1871fda913c6fbd1bd
Jchhm24 <jcchm2004@gmail.com>
2025-01-25 21:17:31 -0600

    agregar paginación a la vista principal en HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="grid grid-cols-(--grid-cols-response) gap-4 py-6 px-30">
     <div
-      v-for="(item, index) in searchFound"
+      v-for="(item, index) in paginatedItems"
       :key="index"
       class="flex flex-row gap-4 border-2 border-gray-600/25 rounded-lg overflow-hidden p-4"
     >
@@ -30,11 +30,58 @@
       </article>
     </div>
   </main>
+
+  <!-- Sección de paginación -->
+  <div class="flex justify-center gap-4 my-4">
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        class="px-4 py-2 bg-sky-500 text-white rounded-lg disabled:opacity-50"
+      >
+        Anterior
+      </button>
+      <span class="py-2">Página {{ currentPage }} de {{ totalPages }}</span>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="px-4 py-2 bg-sky-500 text-white rounded-lg disabled:opacity-50"
+      >
+        Siguiente
+      </button>
+    </div>
+
 </template>
 <script setup lang="ts">
 import { useSearchFoundStore } from '@/stores/useSearchFoundStore'
 import { storeToRefs } from 'pinia'
+import { computed, ref } from 'vue'
 
 const store = useSearchFoundStore()
 const { searchFound } = storeToRefs(store)
+
+// Configuración de la paginación
+const itemsPerPage = 12
+const currentPage = ref<number>(1)
+
+// Cálculos de paginación
+const totalPages = computed(() => Math.ceil(searchFound.value.length / itemsPerPage))
+const paginatedItems = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage
+  const end = start + itemsPerPage
+  return searchFound.value.slice(start, end)
+})
+
+// Métodos de paginación
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++
+  }
+}
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+  }
+}
+
 </script>
