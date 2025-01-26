f6b8ad86cd71b80556fbac7f74bd12a6e27db53d
Jchhm24 <jcchm2004@gmail.com>
2025-01-25 19:27:16 -0600

    agregar encabezado y componente InputSelect en App.vue para mejorar la selección de búsqueda

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <template>
-  <SearchBar />
+  <header class="flex flex-col items-center gap-1 py-4 bg-primary" >
+    <router-link to="/" class="outline-hidden">
+      <h1 class="text-4xl font-black tracking-wider text-white">Found Books</h1>
+    </router-link>
+
+    <section class="flex flex-row gap-4" >
+      <InputSelect
+        :selected="selected"
+        :selectValues="valuesSelect"
+        :functionChangeValue = "setValueSelected"
+        :functionChangeSelect = "setSelected"
+      />
+
+      <SearchBar />
+
+    </section>
+  </header>
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import SearchBar from '@/components/SearchBar.vue'
+import { useSelectSearchStore } from '@/stores/useSelectSearchStore'
+import type { selectOption } from '@/interfaces/selectOption'
+import InputSelect from '@/components/InputSelect.vue'
+
+const valuesSelect : selectOption[] = [
+  {
+    value: 'title',
+    text: 'Título'
+  },
+  {
+    value: 'author',
+    text: 'Autor'
+  },
+  {
+    value: 'isbn',
+    text: 'ISBN'
+  },
+]
+
+const { selected ,setSelected, setValueSelected} = useSelectSearchStore()
 </script>
